ec763e2f4ce74913afdb1e20735a9d9dc6b1f13d
Matt Gleich <git@mattglei.ch>
2025-03-08 00:55:58 -0500

    feat: head icons
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/lib/Head.svelte
@@ -0,0 +1,64 @@
+<!-- DYNAMIC HEAD CONTENT -->
+
+<script lang="ts">
+	let {
+		title,
+		description,
+		keywords = []
+	}: { title: string; description: string; keywords?: string[] } = $props();
+
+	keywords.concat([
+		'matt gleich',
+		'matt',
+		'gleich',
+		'matthew gleich',
+		'matthew',
+		'gleich',
+		'photography',
+		'coding',
+		'cycling',
+		'rit',
+		'stainless',
+		'bottomline technologies',
+		'kcf technologies',
+		'kcf'
+	]);
+
+	const name = 'Matt Gleich';
+
+	const siteName = 'mattglei.ch';
+	const siteURL = `https://${siteName}`;
+
+	const ogImageURL = `${siteURL}/opengraph.png`;
+	const ogImageWidth = '1200';
+	const ogImageHeight = '630';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<link rel="author" href={siteURL} />
+	<meta name="author" content={name} />
+	<meta name="keywords" content={keywords.join(',')} />
+
+	<!-- opengraph -->
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={title} />
+	<meta property="og:url" content={siteURL} />
+	<meta property="og:site_name" content={siteName} />
+	<meta property="og:locale" content="en-US" />
+	<meta property="og:image" content={ogImageURL} />
+	<meta property="og:image:width" content={ogImageWidth} />
+	<meta property="og:image:height" content={ogImageHeight} />
+	<meta property="og:type" content="website" />
+
+	<!-- twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:creator" content="@matt_gleich" />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={ogImageURL} />
+	<meta property="twitter:image:width" content={ogImageWidth} />
+	<meta property="twitter:image:height" content={ogImageHeight} />
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="t">
+	import Head from '$lib/Head.svelte';
+</script>
+
+<Head
+	title="Matt Gleich"
+	description="College Student @ RIT (Rochester Institute of Technology), software engineer, cyclist, and photographer"
+/>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
