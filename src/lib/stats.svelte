44c08769ddbd0b7f944eec11f8c0ecf14384f06c
Matt Gleich <git@mattglei.ch>
2025-03-14 23:59:09 -0400

    feat: render stats for map
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/lib/stats.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	const { stats }: { stats: Map<string, string> } = $props();
+</script>
+
+<div class="container">
+	{#each [...stats] as [valueName, value]}
+		<div class="stat">
+			<p class="value">
+				{value}
+			</p>
+			<p class="value-name">
+				{valueName}
+			</p>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		font-family: 'IBM Plex Mono';
+		font-weight: 600;
+		width: 100%;
+	}
+
+	.stat {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	.value {
+		font-size: 18px;
+	}
+
+	.value-name {
+		font-weight: normal;
+		font-size: 12px;
+		color: grey;
+	}
+</style>
