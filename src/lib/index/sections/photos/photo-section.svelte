c6f250cf78ccaa985790ee522a0972b5b61f3fad
Matt Gleich <git@mattglei.ch>
2025-03-31 15:21:45 -0400

    feat: link to instagram from photo section
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/index/sections/photos/photo-section.svelte
+++ src/lib/index/sections/photos/photo-section.svelte
@@ -9,7 +9,9 @@
 		I've been really into photography for about seven years now. Starting with nature photography,
 		mainly landscapes and macro, I've recently delved into automotive photography, especially during
 		my time college with the RIT car club. I primarily use Nikon gear, with my current camera being
-		a Nikon Z7II. Here is a few of my favorite photo's I've taken over the years:
+		a Nikon Z7II. Below is a few of my favorite photo's I've taken over the years. You can see more
+		of my photos over on my <a href="https://instagram.com/mattglei.ch" target="_blank">Instagram</a
+		>.
 	</p>
 	<PhotoGallery count={6} />
 	<a class="view-more" href="/photos"><ViewButton of="photo gallery" /></a>
