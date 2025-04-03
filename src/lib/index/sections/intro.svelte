e6df625d07775ebfdb4b4076c31019a51c195d55
Matt Gleich <git@mattglei.ch>
2025-04-03 00:12:45 -0400

    feat: rit text and colors
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/index/sections/intro.svelte
+++ src/lib/index/sections/intro.svelte
@@ -1,28 +1,49 @@
 <script>
+	import RitIcon from '$lib/icons/rit-icon.svelte';
 	import Resume from '$lib/resume.svelte';
 </script>
 
 <div class="container">
 	<p>Hey, I'm Matt Gleich</p>
-	<p>
+	<div>
 		I'm a third-year Computer Science student at the
-		<a href="https://rit.edu" target="_blank">Rochester Institute of Technology (RIT)</a>. This
-		website serves as a portfolio showcasing some of my projects, work experience, and personal
-		interests. I am committed to applying my technical skills to develop practical solutions and
-		consistently strive to expand my expertise. If you'd like to discuss a project or explore
-		potential opportunities, please contact me at
+
+		<a href="https://rit.edu" target="_blank" class="rit-link">
+			<span class="rit-icon">
+				<RitIcon />
+			</span>Rochester Institute of Technology (RIT)
+		</a>. This website serves as a portfolio showcasing some of my projects, work experience, and
+		personal interests. I am committed to applying my technical skills to develop practical
+		solutions and consistently strive to expand my expertise. If you'd like to discuss a project or
+		explore potential opportunities, please contact me at
 		<a href="mailto:mail@mattglei.ch">mail@mattglei.ch</a>. Additional details about my professional
 		work are in my <Resume />.
-	</p>
+	</div>
 </div>
 
 <style>
 	.container {
 		display: flex;
 		flex-direction: column;
-		gap: 10px;
+		gap: 5px;
 		margin: 10px;
 		margin-bottom: 40px;
+	}
+
+	.rit-icon {
+		width: 20px;
+		height: 20px;
+		position: relative;
+		bottom: -4px;
+		margin: 0 2px;
+		margin-right: 5px;
+		display: inline-block;
+		color: #f76900;
+	}
+
+	.rit-link {
+		color: #f76900;
+		display: inline-block;
 	}
 
 	@media (max-width: 400px) {
