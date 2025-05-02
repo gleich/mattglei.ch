e76237acdd8e108190ee71a277ca7536c29912bf
Matt Gleich <git@mattglei.ch>
2025-05-02 13:24:29 -0400

    feat: use new layout component
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 	import Nav from '$lib/nav.svelte';
 	import Fonts from '$lib/fonts.svelte';
-	import { FadeUp, Copyright } from '@gleich/ui';
+	import { Layout } from '@gleich/ui';
 	import type { Snippet } from 'svelte';
 
 	const { children }: { children: Snippet } = $props();
@@ -13,43 +13,7 @@
 
 <Fonts />
 
-<div class="container">
-	<FadeUp>
-		<div class="main">
-			<Nav />
-			<div class="children">
-				{@render children()}
-			</div>
-			<div class="copyright">
-				<Copyright repo="gleich/mattglei.ch" />
-			</div>
-		</div>
-	</FadeUp>
-</div>
-
-<style>
-	.container {
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		min-height: 100vh;
-		overflow: hidden;
-	}
-
-	.main {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		flex: 1;
-		max-width: 1000px;
-		padding: 50px 20px;
-	}
-
-	.children {
-		flex: 1;
-	}
-
-	.copyright {
-		margin-top: 60px;
-	}
-</style>
+<Layout repo="gleich/mattglei.ch">
+	<Nav />
+	{@render children()}
+</Layout>
