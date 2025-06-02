d8f07fa7f7dbf057aecb5f0e68dae13def4c1a83
Matt Gleich <git@mattglei.ch>
2025-06-01 23:55:21 -0400

    feat(minor): remove data word
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/writing/lcp/+page.svelte
+++ src/routes/writing/lcp/+page.svelte
@@ -24,7 +24,7 @@
 				<CacheStatus
 					name="Projects"
 					updated={projects?.updated}
-					updateFrequency="Refreshing data every 30 seconds"
+					updateFrequency="Refreshing every 30 seconds"
 				/>
 			{/await}
 			{#await data.workouts}
