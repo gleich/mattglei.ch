1a03394d3d886c6912464d90b6a6f0dbe8342b86
Matt Gleich <git@mattglei.ch>
2025-08-04 16:14:20 -0400

    feat: have lcp requet timeout after 15 seconds

--- src/lib/lcp/lcp.server.ts
+++ src/lib/lcp/lcp.server.ts
@@ -18,10 +18,6 @@ export async function loadFromLCP<T>(
 	cache: Cache,
 	fetch: SvelteFetch
 ): Promise<LcpResponse<T> | null> {
-	// uncomment to check loading animation for each section
-	// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-	// await sleep(1000);
-
 	let pathName: string;
 	switch (cache) {
 		case Cache.Workouts:
@@ -37,18 +33,30 @@ export async function loadFromLCP<T>(
 			pathName = 'applemusic';
 			break;
 	}
+
 	const url = `https://lcp.mattglei.ch/${pathName}`;
+
+	const controller = new AbortController();
+	const timeoutId = setTimeout(() => controller.abort(), 15_000);
+
 	try {
 		const res = await fetch(url, {
 			method: 'GET',
 			cache: 'no-store',
 			headers: {
 				Authorization: `Bearer ${LCP_TOKEN}`
-			}
+			},
+			signal: controller.signal
 		});
+
+		clearTimeout(timeoutId);
 		return await res.json();
-	} catch (error) {
-		console.error(`error when trying to fetch ${url}:`, error);
+	} catch (err: unknown) {
+		if (err instanceof DOMException && err.name === 'AbortError') {
+			console.error(`Timed out after 15 s: ${url}`);
+		} else {
+			console.error(`Error fetching ${url}:`, err);
+		}
 		return null;
 	}
 }
