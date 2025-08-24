12001f526ad0caf0a35ac95d7db2af78d7008d25
Matt Gleich <git@mattglei.ch>
2025-08-24 12:11:05 -0400

    feat: 4th year of college
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/index/sections/intro.svelte
+++ src/lib/index/sections/intro.svelte
@@ -5,7 +5,7 @@
 <div class="container">
 	<p>Hey, I'm Matt Gleich</p>
 	<div>
-		I'm a rising fourth-year computer science student at the
+		I'm a fourth-year computer science student at the
 		<a href="https://rit.edu" target="_blank">Rochester Institute of Technology (RIT)</a>
 		and currently completing a full-time co-op as an Associate DevOps Engineer at
 		<a href="https://kcftech.com" target="_blank">KCF Technologies</a>. This website serves as a
