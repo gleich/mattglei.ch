de1fd2618c1390c93f6334616cf7f98726ea9f57
Matt Gleich <git@mattglei.ch>
2025-10-23 14:41:56 -0400

    feat: etymotic icon
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/writing/audio/+page.svelte
+++ src/routes/writing/audio/+page.svelte
@@ -10,14 +10,14 @@
 			Last fall I found myself listening to a lot of music as I was focusing on my studies. My dad,
 			who is really into music and audio gear, gave me a pair of his old <a
 				href="https://etymotic.com/earphones/"
-				target="_blank">Etymotic</a
+				target="_blank">Etymōtic</a
 			> IEMs from the early 2000s/late 90s. I really loved the isolation high-resolution that provided
 			compared to the Apple Airpods Pro 2 that I was using at the time.
 		</p>
 		<br />
 		<p>
-			For Christmas of last year, only a few months after I started using those Etymotics, I got
-			some new pair of Etymotics as well as my first speaker setup! Here is all of the gear that I
+			For Christmas of last year, only a few months after I started using those Etymōtics, I got
+			some new pair of Etymōtics as well as my first speaker setup! Here is all of the gear that I
 			got then:
 		</p>
 	</WritingSection>
