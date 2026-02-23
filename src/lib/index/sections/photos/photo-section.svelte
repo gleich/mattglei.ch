3876a0f651d758d16b7f4ae7bc5822b6130603f8
Matt Gleich <git@mattglei.ch>
2026-02-23 13:45:12 -0500

    feat: 9 years of photos!

--- src/lib/index/sections/photos/photo-section.svelte
+++ src/lib/index/sections/photos/photo-section.svelte
@@ -6,7 +6,7 @@
 
 <Section name="Photos">
 	<p>
-		I've been really into photography for about seven years now. Starting with nature photography,
+		I've been really into photography for about nine years now. Starting with nature photography,
 		mainly landscapes and macro, I've recently delved into automotive photography, especially during
 		my time college with the RIT car club. I primarily use Nikon gear, with my current camera being
 		a Nikon Z7II. Below is a few of my favorite photo's I've taken over the years. You can see more
