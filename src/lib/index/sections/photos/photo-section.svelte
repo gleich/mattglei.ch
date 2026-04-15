02b1fdefae8350aeac4659d7fb166989d85c6014
Matt Gleich <git@mattglei.ch>
2026-04-15 02:17:43 -0400

    fix[lint]: format with prettier

--- src/lib/index/sections/photos/photo-section.svelte
+++ src/lib/index/sections/photos/photo-section.svelte
@@ -9,11 +9,10 @@
 <Section name="Photos">
 	<p>
 		I've been really into photography for about {yearsSince} years now. Starting with nature photography,
-		mainly landscapes and macro, I've recently delved into automotive photography, especially during
-		my time college with the RIT car club. I primarily use Nikon gear, with my current camera being
-		a Nikon Z7II. Below is a few of my favorite photo's I've taken over the years. You can see more
-		of my photos over on my <a href="https://instagram.com/mattglei.ch" target="_blank">Instagram</a
-		>.
+		mainly landscapes and macro, I've recently delved into automotive photography, especially during my
+		time college with the RIT car club. I primarily use Nikon gear, with my current camera being a Nikon
+		Z7II. Below is a few of my favorite photo's I've taken over the years. You can see more of my photos
+		over on my <a href="https://instagram.com/mattglei.ch" target="_blank">Instagram</a>.
 	</p>
 	<PhotoGallery count={6} />
 	<a class="view-more" href="/photos"><ViewButton of="photo gallery" /></a>
