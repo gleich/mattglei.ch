f7345414a457c9a293b58eda646233b85e9d5e20
Matt Gleich <git@mattglei.ch>
2026-09-05 00:42:59 -0400

    feat: add year after 5th
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/index/sections/intro.svelte
+++ src/lib/index/sections/intro.svelte
@@ -5,7 +5,7 @@
 <div class="container">
 	<p>Hey, I'm Matt Gleich</p>
 	<div>
-		I'm a 5th (5 year program due to
+		I'm a 5th year (5 year program due to
 		<a href="https://www.rit.edu/careerservices/students/co-op" target="_blank">RIT COOP</a>
 		program) computer science student at the
 		<a href="https://rit.edu" target="_blank">Rochester Institute of Technology (RIT)</a>. This
